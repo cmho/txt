10bf003bb17725955e459660ef6f9bf20adc0c5d
Carly Ho <carlymho@gmail.com>
2015-12-10 12:42:10 -0600

    Add post templates and stuff

--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -4,14 +4,27 @@ class PostController < ApplicationController
 	end
 
 	def new
-
+		
 	end
 
 	def create
-
+		@post = Post.new(post_params)
+		if @post.save!
+			flash[:success] = "Update posted!"
+			redirect_to :root
+		else
+			flash[:error] = "There was an error posting your update."
+			redirect_to :new_post_path
+		end
 	end
 
 	def destroy
 
+	end
+
+	private
+
+	def post_params
+		params.require(:post).permit(:content, :user_id, :id)
 	end
 end
